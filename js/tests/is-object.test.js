eab728591f7bf7f180adfa7a6d4d96453608a3b0
Trevor Sears <trevorsears.main@gmail.com>
2019-08-01 15:12:35 -0400

    Added test cases for 'isObject'.

--- js/tests/is-object.test.js
+++ js/tests/is-object.test.js
@@ -1,1 +1,72 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const is_object_1 = require("../is-object");
+describe("'number' should fail check.", () => {
+    describe("Negative number.", () => {
+        test("Integer", () => {
+            expect(is_object_1.isObject(-40)).toBeFalsy();
+        });
+        test("Float", () => {
+            expect(is_object_1.isObject(-11.222)).toBeFalsy();
+        });
+    });
+    test("Zero.", () => {
+        expect(is_object_1.isObject(0)).toBeFalsy();
+    });
+    describe("Positive numbers.", () => {
+        test("Integer", () => {
+            expect(is_object_1.isObject(241)).toBeFalsy();
+        });
+        test("Float", () => {
+            expect(is_object_1.isObject(0.4823)).toBeFalsy();
+        });
+    });
+    test("NaN", () => {
+        expect(is_object_1.isObject(NaN)).toBeFalsy();
+    });
+});
+describe("'string' should fail check.", () => {
+    test("Normal string.", () => {
+        expect(is_object_1.isObject("Hello, world!")).toBeFalsy();
+    });
+    test("Zero-length string.", () => {
+        expect(is_object_1.isObject("")).toBeFalsy();
+    });
+    test("Numeric string.", () => {
+        expect(is_object_1.isObject("1001")).toBeFalsy();
+    });
+    test("Boolean string.", () => {
+        expect(is_object_1.isObject("true")).toBeFalsy();
+    });
+});
+describe("'boolean' should fail check.", () => {
+    test("true", () => {
+        expect(is_object_1.isObject(true)).toBeFalsy();
+    });
+    test("false", () => {
+        expect(is_object_1.isObject(false)).toBeFalsy();
+    });
+});
+describe("Special values should fail check.", () => {
+    test("undefined", () => {
+        expect(is_object_1.isObject(undefined)).toBeFalsy();
+    });
+    test("null", () => {
+        expect(is_object_1.isObject(null)).toBeFalsy();
+    });
+});
+describe("Objects should pass check.", () => {
+    test("Normal object.", () => {
+        expect(is_object_1.isObject({ hello: "there", whats: "up" })).toBeTruthy();
+    });
+    test("Empty object.", () => {
+        expect(is_object_1.isObject({})).toBeTruthy();
+    });
+    test("Normal array.", () => {
+        expect(is_object_1.isObject([1, 3, 5, 7])).toBeTruthy();
+    });
+    test("Empty array.", () => {
+        expect(is_object_1.isObject([])).toBeTruthy();
+    });
+});
 //# sourceMappingURL=is-object.test.js.map
--- ts/tests/is-object.test.ts
+++ ts/tests/is-object.test.ts
@@ -4,6 +4,8 @@
  *	Project: merj
  */
 
+import { isObject } from "../is-object";
+
 /**
  * Test cases for the 'is-object' function.
  *
@@ -12,3 +14,138 @@
  * @since v0.3.0
  */
 
+describe("'number' should fail check.", () => {
+	
+	describe("Negative number.", () => {
+		
+		test("Integer", () => {
+			
+			expect(isObject(-40)).toBeFalsy();
+			
+		});
+		
+		test("Float", () => {
+			
+			expect(isObject(-11.222)).toBeFalsy();
+			
+		});
+		
+	});
+	
+	test("Zero.", () => {
+		
+		expect(isObject(0)).toBeFalsy();
+		
+	});
+	
+	describe("Positive numbers.", () => {
+		
+		test("Integer", () => {
+			
+			expect(isObject(241)).toBeFalsy();
+			
+		});
+		
+		test("Float", () => {
+			
+			expect(isObject(0.4823)).toBeFalsy();
+			
+		});
+		
+	});
+	
+	test("NaN", () => {
+		
+		expect(isObject(NaN)).toBeFalsy();
+		
+	});
+	
+});
+
+describe("'string' should fail check.", () => {
+	
+	test("Normal string.", () => {
+		
+		expect(isObject("Hello, world!")).toBeFalsy();
+		
+	});
+	
+	test("Zero-length string.", () => {
+		
+		expect(isObject("")).toBeFalsy();
+		
+	});
+	
+	test("Numeric string.", () => {
+		
+		expect(isObject("1001")).toBeFalsy();
+		
+	});
+	
+	test("Boolean string.", () => {
+		
+		expect(isObject("true")).toBeFalsy();
+		
+	});
+	
+});
+
+describe("'boolean' should fail check.", () => {
+	
+	test("true", () => {
+		
+		expect(isObject(true)).toBeFalsy();
+		
+	});
+	
+	test("false", () => {
+		
+		expect(isObject(false)).toBeFalsy();
+		
+	});
+	
+});
+
+describe("Special values should fail check.", () => {
+	
+	test("undefined", () => {
+		
+		expect(isObject(undefined)).toBeFalsy();
+		
+	});
+	
+	test("null", () => {
+		
+		expect(isObject(null)).toBeFalsy();
+		
+	});
+	
+});
+
+describe("Objects should pass check.", () => {
+	
+	test("Normal object.", () => {
+		
+		expect(isObject({ hello: "there", whats: "up" })).toBeTruthy();
+		
+	});
+	
+	test("Empty object.", () => {
+		
+		expect(isObject({})).toBeTruthy();
+		
+	});
+	
+	test("Normal array.", () => {
+		
+		expect(isObject([1, 3, 5, 7])).toBeTruthy();
+		
+	});
+	
+	test("Empty array.", () => {
+		
+		expect(isObject([])).toBeTruthy();
+		
+	});
+	
+});
